8c62c6f39de99814209862f2e5b2fec5b49ece56
Pawan Rama Mali <prm@outlook.in>
2025-02-06 23:19:07 +0530

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -6,10 +6,11 @@ ui <- page_sidebar(
     title = "Configuration",
     textInput("model_name", "Enter LLM Model to use:"),
     textInput("openai_api_key", "Enter OpenAI API Key"),
-    input_task_button("confirm_model", label = "Update")
+    input_task_button("confirm_settings", label = "Update")
   ),
   card(
     card_header("The chat's response"),
+    verbatimTextOutput("status"),
     uiOutput("chat_response"),
     textInput("user_query", "Enter query:"),
     input_task_button("ask_chat", label = "Ask the chat")
